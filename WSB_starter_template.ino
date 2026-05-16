f23d2a7bd4b740631e571915a3780570b05ac780
Kripal <56967890+kripalsingh007@users.noreply.github.com>
2026-05-16 14:29:39 +0530

    NeoPixel LED control code refrence
    This code initializes a NeoPixel LED strip and sets all LEDs to red in the setup function.

---
+++ WSB_starter_template.ino
@@ -0,0 +1,26 @@
+#include <Adafruit_NeoPixel.h>
+
+#define LED_PIN    6
+#define LED_COUNT  16   // This defines the number of LED's in your module
+
+Adafruit_NeoPixel strip(LED_COUNT, LED_PIN, NEO_GRB + NEO_KHZ800);
+
+void setup() 
+{
+  strip.begin();
+  
+  // Set all LEDs to red
+  for(int i = 0; i < LED_COUNT; i++) 
+  {
+    strip.setPixelColor(i, strip.Color(255, 0, 0));  // R : Red, G : Green, B : Blue
+  }
+
+  strip.show();
+}
+
+void loop() 
+{
+  // Nothing here
+}
+
+// this is the basic code for using WSB Led's, you can take refrence of this code to complete the pending tasks
